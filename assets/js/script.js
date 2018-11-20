90b1c78617e211f65f2455a7edc681dd4a00e8d5
Shajan Jacob <shajanjp@gmail.com>
2018-11-20 23:01:59 +0530

    moves shuffling to main script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,30 @@
 var i=0;
 var taskContainer = document.getElementsByClassName('wrapper')[0];
 
+function generateRandomNumber(min, max){
+  return Math.floor(Math.random() * (max-min) + min);
+}
+
+function shuffle(ordered){
+  let shuffled = [];
+  for(var i=ordered.length; i>0; i--){
+    let r =generateRandomNumber(0, i);
+    shuffled.push(ordered.splice(r, 1)[0])
+  }
+  return shuffled;
+}
+
+var tasksList = shuffle(tasksCollection);
+
 setTask(generateRandomNumber(0, tasksList.length-1))
 
 function spitSpans(task){
-  return `<p class="fadeInDown animated">${task
-  .split('')
-  .map(stuff => `<span>${stuff}</span>`)
-  .join('')}</p>`;
+  return `<p class="fadeInDown animated">
+    ${task
+    .split('')
+    .map(stuff => `<span>${stuff}</span>`)
+    .join('')}
+    </p>`;
 }
 
 function setTask(taskNumber){
